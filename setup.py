78b27cc64384781a3900679a3bc51beab82b8492
eugenyProchan <pro4anjey@gmail.com>
2020-06-22 10:29:11 +0500

    change setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MyPackageName',
+    name='MyPackageName_1',
     version='1.0.0',
     url='https://github.com/mypackage.git',
     author='Author Name',
     author_email='author@gmail.com',
-    description='Description of my package',
+    description='Description of the package',
     packages=find_packages(),
     install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
 )
