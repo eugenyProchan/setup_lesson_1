85b51f7149489246318abcd384a4ba588e5935bb
eugenyProchan <pro4anjey@gmail.com>
2020-06-22 12:12:58 +0500

    change setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MyPackageName_1',
+    name='MyPackageName_first',
     version='1.0.0',
     url='https://github.com/mypackage.git',
     author='Author Name',
